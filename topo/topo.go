48b3cda3b5aa438d55e6d79d5764f51121057b7b
marcushines <80116818+marcushines@users.noreply.github.com>
2021-05-14 11:52:07 -0700

    Update topo.go
    Remove SRL until next checkin

--- topo/topo.go
+++ topo/topo.go
@@ -40,7 +40,6 @@ import (
 	_ "github.com/google/kne/topo/node/frr"
 	_ "github.com/google/kne/topo/node/host"
 	_ "github.com/google/kne/topo/node/quagga"
-	_ "github.com/google/kne/topo/node/srl"
 )
 
 var (
