85670fdcebce23d0f91e7402951285302c933ea4
Marcus Hines <hines@google.com>
2021-05-20 18:11:45 +0000

    Add Keysight Ixia TG and regenerate topo proto

---
+++ topo/node/ixia/ixia.go
@@ -0,0 +1,43 @@
+package ixia
+
+import (
+	"fmt"
+
+	topopb "github.com/google/kne/proto/topo"
+	"github.com/google/kne/topo/node"
+	"google.golang.org/protobuf/proto"
+)
+
+func New(pb *topopb.Node) (node.Interface, error) {
+	cfg := defaults(pb)
+	proto.Merge(cfg, pb)
+	node.FixServices(cfg)
+	return &Node{
+		pb: cfg,
+	}, nil
+
+}
+
+type Node struct {
+	pb *topopb.Node
+}
+
+func (n *Node) Proto() *topopb.Node {
+	return n.pb
+}
+
+func defaults(pb *topopb.Node) *topopb.Node {
+	return &topopb.Node{
+		Config: &topopb.Config{
+			Image:        "alpine:latest",
+			Command:      []string{"/bin/sh", "-c", "sleep 2000000000000"},
+			EntryCommand: fmt.Sprintf("kubectl exec -it %s -- sh", pb.Name),
+			ConfigPath:   "/etc",
+			ConfigFile:   "config",
+		},
+	}
+}
+
+func init() {
+	node.Register(topopb.Node_IxiaTG, New)
+}
