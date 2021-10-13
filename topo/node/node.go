634a1af0276c55b427deec50b8d0be8b9d0e6b0f
alexmasi <alexmasi@google.com>
2021-10-13 15:08:17 -0700

    add init_container option to node proto

--- topo/node/node.go
+++ topo/node/node.go
@@ -118,7 +118,7 @@ func (n *Impl) GetNamespace() string {
 }
 
 const (
-	InitContainerName = "networkop/init-wait:latest"
+	defaultInitContainerName = "networkop/init-wait:latest"
 )
 
 func ToEnvVar(kv map[string]string) []corev1.EnvVar {
@@ -196,6 +196,10 @@ func (n *Impl) CreateConfig(ctx context.Context) error {
 func (n *Impl) CreatePod(ctx context.Context) error {
 	pb := n.Proto
 	log.Infof("Creating Pod:\n %+v", pb)
+	initContainerName := pb.Config.InitContainer
+	if initContainerName == "" {
+		initContainerName = defaultInitContainerName
+	}
 	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: pb.Name,
@@ -207,7 +211,7 @@ func (n *Impl) CreatePod(ctx context.Context) error {
 		Spec: corev1.PodSpec{
 			InitContainers: []corev1.Container{{
 				Name:  fmt.Sprintf("init-%s", pb.Name),
-				Image: InitContainerName,
+				Image: initContainerName,
 				Args: []string{
 					fmt.Sprintf("%d", len(n.Proto.Interfaces)+1),
 					fmt.Sprintf("%d", pb.Config.Sleep),
