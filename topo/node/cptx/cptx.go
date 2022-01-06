e7f435e98e1919fcf41334cf59a01e4d0832298b
Nitin Soni <nsoni@juniper.net>
2022-01-06 09:01:08 -0800

    cptx k8s operator alternative (#80)
    Adds some customizations to cptx without the need of a k8s operator.
    Also changes the cptx kne config to adapt to the latest of KNE.

--- topo/node/cptx/cptx.go
+++ topo/node/cptx/cptx.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"strings"
 	"time"
 
 	tpb "github.com/google/kne/proto/topo"
@@ -21,6 +22,9 @@ import (
 	scraplitest "github.com/scrapli/scrapligo/util/testhelper"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/proto"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 )
 
 // ErrIncompatibleCliConn raised when an invalid scrapligo cli transport type is found.
@@ -33,7 +37,14 @@ var (
 	scrapliOperationTimeout = 300 * time.Second
 )
 
+const (
+	defaultInitContainerImage = "networkop/init-wait:latest"
+)
+
 func New(impl *node.Impl) (node.Node, error) {
+	if impl == nil {
+		return nil, fmt.Errorf("nodeImpl cannot be nil")
+	}
 	cfg := defaults(impl.Proto)
 	proto.Merge(cfg, impl.Proto)
 	node.FixServices(cfg)
@@ -192,8 +203,144 @@ func (n *Node) ConfigPush(ctx context.Context, r io.Reader) error {
 	return nil
 }
 
+func (n *Node) Create(ctx context.Context) error {
+	log.Infof("Creating cPTX node resource %s", n.Name())
+
+	if err := n.CreateConfig(ctx); err != nil {
+		return fmt.Errorf("node %s failed to create config-map %w", n.Name(), err)
+	}
+	log.Infof("Created cPTX node %s configmap", n.Name())
+
+	pb := n.Proto
+	initContainerImage := pb.Config.InitImage
+	if initContainerImage == "" {
+		initContainerImage = defaultInitContainerImage
+	}
+
+	// downward api - pass some useful values to container
+	if n.isChannelized() {
+		pb.Config.Env["CPTX_CHANNELIZED"] = "1"
+	}
+	pb.Config.Env["CPTX_CPU_LIMIT"] = pb.Constraints["cpu"]
+	pb.Config.Env["CPTX_MEMORY_LIMIT"] = pb.Constraints["memory"]
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: n.Name(),
+			Labels: map[string]string{
+				"app":  n.Name(),
+				"topo": n.Namespace,
+			},
+		},
+		Spec: corev1.PodSpec{
+			InitContainers: []corev1.Container{{
+				Name:  fmt.Sprintf("init-%s", n.Name()),
+				Image: initContainerImage,
+				Args: []string{
+					fmt.Sprintf("%d", len(pb.GetInterfaces())+1),
+					fmt.Sprintf("%d", pb.GetConfig().Sleep),
+				},
+				ImagePullPolicy: "IfNotPresent",
+			}},
+			Containers: []corev1.Container{{
+				Name:            n.Name(),
+				Image:           pb.Config.Image,
+				Command:         pb.Config.Command,
+				Args:            pb.Config.Args,
+				Env:             node.ToEnvVar(pb.Config.Env),
+				Resources:       node.ToResourceRequirements(pb.Constraints),
+				ImagePullPolicy: "IfNotPresent",
+				SecurityContext: &corev1.SecurityContext{
+					Privileged: pointer.Bool(true),
+					RunAsUser:  pointer.Int64(0),
+					Capabilities: &corev1.Capabilities{
+						Add: []corev1.Capability{"SYS_ADMIN"},
+					},
+				},
+				VolumeMounts: []corev1.VolumeMount{{
+					Name:      fmt.Sprintf("%s-run-mount", pb.Name),
+					ReadOnly:  false,
+					MountPath: "/run",
+				}},
+			}},
+			Volumes: []corev1.Volume{{
+				Name: fmt.Sprintf("%s-run-mount", pb.Name),
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{
+						Medium: "Memory",
+					},
+				},
+			}},
+			TerminationGracePeriodSeconds: pointer.Int64(0),
+			NodeSelector:                  map[string]string{},
+			Affinity: &corev1.Affinity{
+				PodAntiAffinity: &corev1.PodAntiAffinity{
+					PreferredDuringSchedulingIgnoredDuringExecution: []corev1.WeightedPodAffinityTerm{{
+						Weight: 100,
+						PodAffinityTerm: corev1.PodAffinityTerm{
+							LabelSelector: &metav1.LabelSelector{
+								MatchExpressions: []metav1.LabelSelectorRequirement{{
+									Key:      "topo",
+									Operator: "In",
+									Values:   []string{pb.Name},
+								}},
+							},
+							TopologyKey: "kubernetes.io/hostname",
+						},
+					}},
+				},
+			},
+		},
+	}
+	if pb.Config.ConfigData != nil {
+		pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
+			Name: "startup-config-volume",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: fmt.Sprintf("%s-config", pb.Name),
+					},
+				},
+			},
+		})
+		for i, c := range pod.Spec.Containers {
+			pod.Spec.Containers[i].VolumeMounts = append(c.VolumeMounts, corev1.VolumeMount{
+				Name:      "startup-config-volume",
+				MountPath: pb.Config.ConfigPath + "/" + pb.Config.ConfigFile,
+				SubPath:   pb.Config.ConfigFile,
+				ReadOnly:  true,
+			})
+		}
+	}
+	sPod, err := n.KubeClient.CoreV1().Pods(n.Namespace).Create(ctx, pod, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to create pod for %q: %w", pb.Name, err)
+	}
+	log.Debugf("Pod created:\n%+v\n", sPod)
+	log.Infof("Created cPTX node resource %s pod", n.Name())
+	if err := n.CreateService(ctx); err != nil {
+		return err
+	}
+	log.Infof("Created cPTX node resource %s services", n.Name())
+	return nil
+}
+
 func defaults(pb *tpb.Node) *tpb.Node {
+	if pb == nil {
+		pb = &tpb.Node{
+			Name: "default_cptx_node",
+		}
+	}
+	if pb.Config == nil {
+		pb.Config = &tpb.Config{}
+	}
+	if pb.Config.ConfigFile == "" {
+		pb.Config.ConfigFile = "juniper.conf"
+	}
+	if pb.Config.ConfigPath == "" {
+		pb.Config.ConfigPath = "/home/evo/configdisk"
+	}
 	return &tpb.Node{
+		Name: pb.Name,
 		Constraints: map[string]string{
 			"cpu":    "8",
 			"memory": "8Gi",
@@ -216,7 +363,8 @@ func defaults(pb *tpb.Node) *tpb.Node {
 			},
 		},
 		Labels: map[string]string{
-			"type": tpb.Node_JUNIPER_CEVO.String(),
+			"type":   tpb.Node_JUNIPER_CEVO.String(),
+			"vendor": tpb.Vendor_JUNIPER.String(),
 		},
 		Config: &tpb.Config{
 			Image: "cptx:latest",
@@ -227,10 +375,21 @@ func defaults(pb *tpb.Node) *tpb.Node {
 				"CPTX": "1",
 			},
 			EntryCommand: fmt.Sprintf("kubectl exec -it %s -- cli -c", pb.Name),
-			ConfigPath:   "/home/evo/configdisk",
-			ConfigFile:   "juniper.conf",
+			ConfigPath:   pb.Config.ConfigPath,
+			ConfigFile:   pb.Config.ConfigFile,
 		},
 	}
+}
+
+// isChannelized is a helper function that returns 1 if cptx is channelized
+func (n *Node) isChannelized() bool {
+	interfaces := n.GetProto().GetInterfaces()
+	for key, value := range interfaces {
+		if strings.Contains(key, "eth") && strings.Contains(value.Name, ":") {
+			return true
+		}
+	}
+	return false
 }
 
 func init() {
