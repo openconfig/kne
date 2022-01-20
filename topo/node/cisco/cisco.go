fc1b54fc51cb2c63257466c7b1854a017d1ded9f
mojiiba <mbagherz@cisco.com>
2022-01-20 18:55:32 -0500

    improve cisco nodes

--- topo/node/cisco/cisco.go
+++ topo/node/cisco/cisco.go
@@ -11,12 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package xrd
+package cisco
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
 
 	"github.com/google/kne/topo/node"
 	log "github.com/sirupsen/logrus"
@@ -47,14 +51,25 @@ type Node struct {
 }
 
 func (n *Node) Create(ctx context.Context) error {
-	log.Infof("Creating XRD node resource %s", n.Name())
+	log.Infof("Creating Cisco %s node resource %s", n.Proto.Model, n.Name())
 
 	if err := n.CreateConfig(ctx); err != nil {
 		return fmt.Errorf("node %s failed to create config-map %w", n.Name(), err)
 	}
-	log.Infof("Created XRD node %s configmap", n.Name())
-
+	log.Infof("Created Cisco %s node %s configmap", n.Proto.Model, n.Name())
 	pb := n.Proto
+	secContext := &corev1.SecurityContext{
+		Privileged: pointer.Bool(true),
+	}
+	if pb.Model == "xrd" {
+		secContext = &corev1.SecurityContext{
+			Privileged: pointer.Bool(true),
+			RunAsUser:  pointer.Int64(0),
+			Capabilities: &corev1.Capabilities{
+				Add: []corev1.Capability{"SYS_ADMIN"},
+			},
+		}
+	}
 	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: n.Name(),
@@ -81,13 +96,7 @@ func (n *Node) Create(ctx context.Context) error {
 				Env:             node.ToEnvVar(pb.Config.Env),
 				Resources:       node.ToResourceRequirements(pb.Constraints),
 				ImagePullPolicy: "IfNotPresent",
-				SecurityContext: &corev1.SecurityContext{
-					Privileged: pointer.Bool(true),
-					RunAsUser:  pointer.Int64(0),
-					Capabilities: &corev1.Capabilities{
-						Add: []corev1.Capability{"SYS_ADMIN"},
-					},
-				},
+				SecurityContext: secContext,
 				VolumeMounts: []corev1.VolumeMount{{
 					Name:      fmt.Sprintf("%s-run-mount", pb.Name),
 					ReadOnly:  false,
@@ -148,18 +157,111 @@ func (n *Node) Create(ctx context.Context) error {
 		return fmt.Errorf("failed to create pod for %q: %w", pb.Name, err)
 	}
 	log.Debugf("Pod created:\n%+v\n", sPod)
-	log.Infof("Created XRD node resource %s pod", n.Name())
+	log.Infof("Created Cisco %s node resource %s pod", n.Proto.Model, n.Name())
 	if err := n.CreateService(ctx); err != nil {
 		return err
 	}
-	log.Infof("Created XRD node resource %s services", n.Name())
+	log.Infof("Created Cisco %s node resource %s services", n.Proto.Model, n.Name())
 	return nil
+}
+
+func constraints(pb *tpb.Node) map[string]string {
+	constraints := map[string]string{
+		"cpu":    "1",
+		"memory": "2Gi",
+	}
+	switch pb.Model {
+	case "8201", "8201-32FH", "8202", "8101-32H", "8102-64H":
+		constraints = map[string]string{
+			"cpu":    "4",
+			"memory": "12Gi",
+		}
+	case "xrd":
+		constraints = map[string]string{
+			"cpu":    "1",
+			"memory": "2Gi",
+		}
+	}
+	return constraints
+}
+
+func getCiscoInterfaceId(model string, ethID int) (string, error) {
+	ciscoInterfacePrefix := ""
+	switch model {
+	case "8201":
+		if ethID-1 <= 23 {
+			ciscoInterfacePrefix = "FourHundredGigE0/0/0/"
+
+		} else if ethID-1 <= 35 {
+			ciscoInterfacePrefix = "HundredGigE0/0/0/"
+		}
+	case "8202":
+		if ethID-1 <= 47 {
+			ciscoInterfacePrefix = "HundredGigE0/0/0/"
+		} else if ethID-1 <= 59 {
+			ciscoInterfacePrefix = "FourHundredGigE0/0/0/"
+		} else if ethID-1 <= 71 {
+			ciscoInterfacePrefix = "HundredGigE0/0/0/"
+		}
+	case "8201-32FH":
+		if ethID-1 <= 31 {
+			ciscoInterfacePrefix = "FourHundredGigE0/0/0/"
+		}
+	case "8101-32H":
+		if ethID-1 <= 31 {
+			ciscoInterfacePrefix = "HundredGigE0/0/0/"
+		}
+	case "8102-64H":
+		if ethID-1 <= 63 {
+			ciscoInterfacePrefix = "HundredGigE0/0/0/"
+		}
+	default:
+		ciscoInterfacePrefix = "GigabitEthernet0/0/0/"
+	}
+	if ciscoInterfacePrefix != "" {
+		return fmt.Sprintf("%s%d", ciscoInterfacePrefix, ethID-1), nil
+	} else {
+		return "", errors.New("invalid interface id")
+	}
+}
+
+func generateInterfacesEnvs(pb *tpb.Node) (interfaceList, interfaceMap string) {
+	interfaceList = "MgmtEther0/RP0/CPU0/0"
+	interfaceMap = "MgmtEther0/RP0/CPU0/0:eth0"
+	eths := make([]string, 0)
+	for k := range pb.Interfaces {
+		eths = append(eths, k)
+	}
+	sort.Strings(eths)
+	ethWithIdRegx := regexp.MustCompile(`e(t(h(e(r(n(e(t)*)*)*)*)*)*)\d+`) // check for e|et|eth|....
+	ethRegx := regexp.MustCompile(`e(t(h(e(r(n(e(t)*)*)*)*)*)*)`)          // check for e|et|eth|....
+	for _, eth := range eths {
+		if !ethWithIdRegx.MatchString(eth) {
+			log.Warn(fmt.Sprintf("Interface id %s is invalid", eth))
+			continue
+		}
+		if pb.Interfaces[eth].Name == "" {
+			ethId, _ := strconv.Atoi(ethRegx.Split(eth, -1)[1])
+			ciscoInterfaceId, err := getCiscoInterfaceId(pb.Model, ethId)
+			if err == nil {
+				interfaceList = fmt.Sprintf("%s,%s", interfaceList, ciscoInterfaceId)
+				interfaceMap = fmt.Sprintf("%s,%s:%s", interfaceMap, ciscoInterfaceId, eth)
+			} else {
+				log.Warn(fmt.Errorf("can not get cisco interface id for %s : %w ", eth, err))
+				continue
+			}
+		} else {
+			interfaceList = fmt.Sprintf("%s,%s", interfaceList, pb.Interfaces[eth].Name)
+			interfaceMap = fmt.Sprintf("%s,%s:%s", interfaceMap, pb.Interfaces[eth].Name, eth)
+		}
+	}
+	return interfaceList, interfaceMap
 }
 
 func defaults(pb *tpb.Node) *tpb.Node {
 	if pb == nil {
 		pb = &tpb.Node{
-			Name: "default_xrd_node",
+			Name: "default_cisco_node",
 		}
 	}
 	if pb.Config == nil {
@@ -171,32 +273,33 @@ func defaults(pb *tpb.Node) *tpb.Node {
 	if pb.Config.ConfigPath == "" {
 		pb.Config.ConfigPath = "/"
 	}
+	if pb.Model == "" {
+		pb.Model = "xrd"
+	}
+	constraints := constraints(pb)
 	if pb.Constraints == nil {
-		pb.Constraints = map[string]string{
-			"cpu":    "1",
-			"memory": "2Gi",
-		}
+		pb.Constraints = constraints
 	} else {
 		if pb.Constraints["cpu"] == "" {
-			pb.Constraints["cpu"] = "1"
+			pb.Constraints["cpu"] = constraints["cpu"]
 		}
 		if pb.Constraints["memory"] == "" {
-			pb.Constraints["memory"] = "2Gi"
+			pb.Constraints["memory"] = constraints["memory"]
 		}
 	}
 	if pb.Services == nil {
 		pb.Services = map[uint32]*tpb.Service{
 			443: {
-				Name:     "ssl",
-				Inside:   443,
+				Name:   "ssl",
+				Inside: 443,
 			},
 			22: {
-				Name:     "ssh",
-				Inside:   22,
+				Name:   "ssh",
+				Inside: 22,
 			},
 			6030: {
-				Name:     "gnmi",
-				Inside:   57400,
+				Name:   "gnmi",
+				Inside: 57400,
 			},
 		}
 	}
@@ -211,27 +314,32 @@ func defaults(pb *tpb.Node) *tpb.Node {
 	if pb.Config.EntryCommand == "" {
 		pb.Config.EntryCommand = fmt.Sprintf("kubectl exec -it %s -- bash", pb.Name)
 	}
+	interfaceList, interfaceMap := generateInterfacesEnvs(pb)
 	if pb.Config.Env == nil {
 		pb.Config.Env = map[string]string{
-			"XR_INTERFACES":                  "Gi0/0/0/0:eth1",
-			"XR_CHECKSUM_OFFLOAD_COUNTERACT": "GigabitEthernet0/0/0/0",
+			"XR_INTERFACES":                  interfaceMap,
+			"XR_CHECKSUM_OFFLOAD_COUNTERACT": interfaceList,
 			"XR_EVERY_BOOT_CONFIG":           filepath.Join(pb.Config.ConfigPath, pb.Config.ConfigFile),
 		}
 	} else {
 		if pb.Config.Env["XR_INTERFACES"] == "" {
-			pb.Config.Env["XR_INTERFACES"] = "Gi0/0/0/0:eth1"
+			pb.Config.Env["XR_INTERFACES"] = interfaceMap
 		}
 		if pb.Config.Env["XR_CHECKSUM_OFFLOAD_COUNTERACT"] == "" {
-			pb.Config.Env["XR_CHECKSUM_OFFLOAD_COUNTERACT"] = "GigabitEthernet0/0/0/0"
+			pb.Config.Env["XR_CHECKSUM_OFFLOAD_COUNTERACT"] = interfaceList
 		}
 		if pb.Config.Env["XR_EVERY_BOOT_CONFIG"] == "" {
 			pb.Config.Env["XR_EVERY_BOOT_CONFIG"] = filepath.Join(pb.Config.ConfigPath, pb.Config.ConfigFile)
 		}
 	}
+	if pb.Model == "xrd" {
+		// XR_SNOOP_IP_INTERFACES should always set to MgmtEther0/RP0/CPU0/0
+		// This enables autmatic bringup of the managment interface for xrd
+		pb.Config.Env["XR_SNOOP_IP_INTERFACES"] = "MgmtEther0/RP0/CPU0/0"
+	}
 	return pb
 }
 
 func init() {
-	node.Register(tpb.Node_CISCO_XRD, New)
 	node.Vendor(tpb.Vendor_CISCO, New)
 }
