f0f7f42a69acba8411241b0d23d2d7b4afef45ce
Ashutosh Kumar <ashutosh.kumar@keysight.com>
2022-01-23 14:32:48 +0530

    added support for loading JSON/YAML files for topolofy (with .yaml, .yml and .json extensions)

---
+++ topo/topo_test.go
@@ -0,0 +1,43 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package topo
+
+import (
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	type args struct {
+		fName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "pb", args: args{fName: "../examples/2node-ixia.pb.txt"}, wantErr: false},
+		{name: "json", args: args{fName: "../examples/2node-ixia.json"}, wantErr: false},
+		{name: "yaml", args: args{fName: "../examples/2node-ixia.yaml"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Load(tt.args.fName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
