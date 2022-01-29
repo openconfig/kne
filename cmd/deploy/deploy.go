41d4d559a96399c2055dd47d5356cb8525285a17
Shubh Mondal <shubhmondal@google.com>
2022-01-29 00:28:06 +0000

    bare bone controller server with unimplemented rpc's

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -71,7 +71,7 @@ func NewDeployment(cfg *DeploymentConfig) (*Deployment, error) {
 	d := &Deployment{}
 	switch cfg.Cluster.Kind {
 	case "Kind":
-		log.Infof("Using kind scenario") // Entry point
+		log.Infof("Using kind scenario")
 		v := &KindSpec{}
 		if err := cfg.Cluster.Spec.Decode(v); err != nil {
 			return nil, err
