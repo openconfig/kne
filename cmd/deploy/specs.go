40e6740df24a5da780c338ab082b164586104e34
Shubh Mondal <shubhmondal@google.com>
2022-02-04 00:13:27 +0000

    Refactor deploy into its own package so common code can be used by controller service

--- cmd/deploy/specs.go
+++ cmd/deploy/specs.go
@@ -14,43 +14,16 @@
 package deploy
 
 import (
-	"bytes"
 	"context"
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
 	"io"
-	"net"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"text/template"
 	"time"
 
-	dtypes "github.com/docker/docker/api/types"
 	dclient "github.com/docker/docker/client"
-	kexec "github.com/google/kne/os/exec"
-	"github.com/pkg/errors"
+	"github.com/google/kne/deploy"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/kind/pkg/cluster"
-)
-
-const (
-	dockerConfigEnvVar           = "DOCKER_CONFIG"
-	kubeletConfigPathTemplate    = "%s:/var/lib/kubelet/config.json"
-	dockerConfigTemplateContents = `{
-  "auths": {
-{{range $val := .}}    "{{$val}}": {}
-{{end}}  }
-}
-`
 )
 
 type ClusterSpec struct {
@@ -93,148 +66,12 @@ type execerInterface interface {
 }
 
 var (
-	dockerConfigTemplate = template.Must(template.New("dockerConfig").Parse(dockerConfigTemplateContents))
-
 	logOut = log.StandardLogger().Out
-
-	// execer handles all execs on host.
-	execer execerInterface = kexec.NewExecer(logOut, logOut)
-
-	// Stubs for testing.
-	newProvider  = defaultProvider
-	execLookPath = exec.LookPath
 )
 
-func defaultProvider() provider {
-	return cluster.NewProvider(cluster.ProviderWithLogger(&logAdapter{log.StandardLogger()}))
-}
-
 func (k *KindSpec) Deploy(ctx context.Context) error {
-	provider := newProvider()
-	if k.Recycle {
-		clusters, err := provider.List()
-		if err != nil {
-			return err
-		}
-		for _, v := range clusters {
-			if k.Name == v {
-				log.Infof("Recycling existing cluster: %s", v)
-				return nil
-			}
-		}
-	}
-	if k.DeployWithClient {
-		if len(k.GoogleArtifactRegistries) != 0 {
-			return fmt.Errorf("setting up access to artifact registries %v requires unsetting the deployWithClient field", k.GoogleArtifactRegistries)
-		}
-		if err := provider.Create(
-			k.Name,
-			cluster.CreateWithNodeImage(k.Image),
-			cluster.CreateWithRetain(k.Retain),
-			cluster.CreateWithWaitForReady(k.Wait),
-			cluster.CreateWithKubeconfigPath(k.Kubecfg),
-			cluster.CreateWithDisplayUsage(true),
-			cluster.CreateWithDisplaySalutation(true),
-		); err != nil {
-			return errors.Wrap(err, "failed to create cluster using kind client")
-		}
-		log.Infof("Deployed kind cluster using kind client: %s", k.Name)
-		return nil
-	}
-	if _, err := execLookPath("kind"); err != nil {
-		return errors.Wrap(err, "install kind cli to deploy, or set the deployWithClient field")
-	}
-	args := []string{"create", "cluster"}
-	if k.Name != "" {
-		args = append(args, "--name", k.Name)
-	}
-	if k.Image != "" {
-		args = append(args, "--image", k.Image)
-	}
-	if k.Retain {
-		args = append(args, "--retain")
-	}
-	if k.Wait != 0 {
-		args = append(args, "--wait", k.Wait.String())
-	}
-	if k.Kubecfg != "" {
-		args = append(args, "--kubeconfig", k.Kubecfg)
-	}
-	if err := execer.Exec("kind", args...); err != nil {
-		return errors.Wrap(err, "failed to create cluster using cli")
-	}
-	log.Infof("Deployed kind cluster: %s", k.Name)
-	return k.setupGoogleArtifactRegistryAccess()
-}
-
-func (k *KindSpec) setupGoogleArtifactRegistryAccess() error {
-	if len(k.GoogleArtifactRegistries) == 0 {
-		log.Debug("No registries require setup")
-		return nil
-	}
-	// Create a temporary dir to hold a new docker config that lacks credsStore.
-	// Then use `docker login` to store the generated credentials directly in
-	// the temporary docker config.
-	// See https://kind.sigs.k8s.io/docs/user/private-registries/#use-an-access-token
-	// for more information.
-	tempDockerDir, err := os.MkdirTemp("", "kne_kind_docker")
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(tempDockerDir)
-	originalConfig := os.Getenv(dockerConfigEnvVar)
-	defer os.Setenv(dockerConfigEnvVar, originalConfig)
-	if err := os.Setenv(dockerConfigEnvVar, tempDockerDir); err != nil {
-		return err
-	}
-	configPath := filepath.Join(tempDockerDir, "config.json")
-	if err := writeDockerConfig(configPath, k.GoogleArtifactRegistries); err != nil {
-		return err
-	}
-	var token bytes.Buffer
-	execer.SetStdout(&token)
-	if err := execer.Exec("gcloud", "auth", "print-access-token"); err != nil {
-		return err
-	}
-	execer.SetStdout(log.StandardLogger().Out)
-	for _, r := range k.GoogleArtifactRegistries {
-		s := fmt.Sprintf("https://%s", r)
-		if err := execer.Exec("docker", "login", "-u", "oauth2accesstoken", "-p", token.String(), s); err != nil {
-			return err
-		}
-	}
-	args := []string{"get", "nodes"}
-	if k.Name != "" {
-		args = append(args, "--name", k.Name)
-	}
-	var nodes bytes.Buffer
-	execer.SetStdout(&nodes)
-	if err := execer.Exec("kind", args...); err != nil {
-		return err
-	}
-	execer.SetStdout(log.StandardLogger().Out)
-	// Copy the new docker config to each node and restart kubelet so it
-	// picks up the new config that contains the embedded credentials.
-	for _, node := range strings.Split(nodes.String(), " ") {
-		node = strings.TrimSuffix(node, "\n")
-		if err := execer.Exec("docker", "cp", configPath, fmt.Sprintf(kubeletConfigPathTemplate, node)); err != nil {
-			return err
-		}
-		if err := execer.Exec("docker", "exec", node, "systemctl", "restart", "kubelet.service"); err != nil {
-			return err
-		}
-	}
-	log.Infof("Setup credentials for accessing GAR locations %v in kind cluster", k.GoogleArtifactRegistries)
-	return nil
-}
-
-func writeDockerConfig(path string, registries []string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	return dockerConfigTemplate.Execute(f, registries)
+	d := &deploy.KindSpec{}
+	return d.Deploy(ctx)
 }
 
 type MetalLBSpec struct {
@@ -249,159 +86,14 @@ func (m *MetalLBSpec) SetKClient(c kubernetes.Interface) {
 	m.kClient = c
 }
 
-func inc(ip net.IP, cnt int) {
-	for cnt > 0 {
-		for j := len(ip) - 1; j >= 0; j-- {
-			ip[j]++
-			if ip[j] > 0 {
-				break
-			}
-		}
-		cnt--
-	}
-}
-
-type pool struct {
-	Name      string   `yaml:"name"`
-	Protocol  string   `yaml:"protocol"`
-	Addresses []string `yaml:"addresses"`
-}
-
-type metalLBConfig struct {
-	AddressPools []pool `yaml:"address-pools"`
-}
-
-func makeConfig(n *net.IPNet, count int) metalLBConfig {
-	start := make(net.IP, len(n.IP))
-	copy(start, n.IP)
-	inc(start, 50)
-	end := make(net.IP, len(start))
-	copy(end, start)
-	inc(end, count)
-	return metalLBConfig{
-		AddressPools: []pool{{
-			Name:      "default",
-			Protocol:  "layer2",
-			Addresses: []string{fmt.Sprintf("%s - %s", start, end)},
-		}},
-	}
-}
-
 func (m *MetalLBSpec) Deploy(ctx context.Context) error {
-	if m.dClient == nil {
-		var err error
-		m.dClient, err = dclient.NewClientWithOpts(dclient.FromEnv)
-		if err != nil {
-			return err
-		}
-	}
-	mPath := filepath.Join(deploymentBasePath, m.ManifestDir)
-	log.Infof("Deploying metallb from: %s", mPath)
-	log.Infof("Creating metallb namespace")
-	if err := execer.Exec("kubectl", "apply", "-f", filepath.Join(mPath, "namespace.yaml")); err != nil {
-		return err
-	}
-	_, err := m.kClient.CoreV1().Secrets("metallb-system").Get(ctx, "memberlist", metav1.GetOptions{})
-	if err != nil {
-		log.Infof("Creating metallb secret")
-		d := make([]byte, 16)
-		rand.Read(d)
-		s := &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "memberlist",
-			},
-			StringData: map[string]string{
-				"secretkey": base64.StdEncoding.EncodeToString(d),
-			},
-		}
-		_, err := m.kClient.CoreV1().Secrets("metallb-system").Create(ctx, s, metav1.CreateOptions{})
-		if err != nil {
-			return err
-		}
-	}
-	log.Infof("Applying metallb pods")
-	if err := execer.Exec("kubectl", "apply", "-f", filepath.Join(mPath, "metallb.yaml")); err != nil {
-		return err
-	}
-	_, err = m.kClient.CoreV1().ConfigMaps("metallb-system").Get(ctx, "config", metav1.GetOptions{})
-	if err != nil {
-		log.Infof("Applying metallb ingress config")
-		// Get Network information from docker.
-		nr, err := m.dClient.NetworkList(ctx, dtypes.NetworkListOptions{})
-		if err != nil {
-			return err
-		}
-		var network dtypes.NetworkResource
-		for _, v := range nr {
-			if v.Name == "kind" {
-				network = v
-				break
-			}
-		}
-		var n *net.IPNet
-		for _, ipRange := range network.IPAM.Config {
-			_, ipNet, err := net.ParseCIDR(ipRange.Subnet)
-			if err != nil {
-				return err
-			}
-			if ipNet.IP.To4() != nil {
-				n = ipNet
-				break
-			}
-		}
-		if n == nil {
-			return fmt.Errorf("failed to find kind ipv4 docker net")
-		}
-		config := makeConfig(n, m.IPCount)
-		b, err := yaml.Marshal(config)
-		if err != nil {
-			return err
-		}
-		cm := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "config",
-			},
-			Data: map[string]string{
-				"config": string(b),
-			},
-		}
-		_, err = m.kClient.CoreV1().ConfigMaps("metallb-system").Create(ctx, cm, metav1.CreateOptions{})
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	d := &deploy.MetalLBSpec{}
+	return d.Deploy(ctx)
 }
 
 func (m *MetalLBSpec) Healthy(ctx context.Context) error {
-	log.Infof("Waiting on Metallb to be Healthy")
-	w, err := m.kClient.AppsV1().Deployments("metallb-system").Watch(ctx, metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	ch := w.ResultChan()
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context canceled before healthy")
-		case e, ok := <-ch:
-			if !ok {
-				return fmt.Errorf("watch channel closed before healthy")
-			}
-			d, ok := e.Object.(*appsv1.Deployment)
-			if !ok {
-				return fmt.Errorf("invalid object type: %T", d)
-			}
-			if d.Status.AvailableReplicas == 1 &&
-				d.Status.ReadyReplicas == 1 &&
-				d.Status.UnavailableReplicas == 0 &&
-				d.Status.Replicas == 1 &&
-				d.Status.UpdatedReplicas == 1 {
-				log.Infof("Metallb Healthy")
-				return nil
-			}
-		}
-	}
+	h := &deploy.MetalLBSpec{}
+	return h.Healthy(ctx)
 }
 
 type MeshnetSpec struct {
@@ -415,40 +107,11 @@ func (m *MeshnetSpec) SetKClient(c kubernetes.Interface) {
 }
 
 func (m *MeshnetSpec) Deploy(ctx context.Context) error {
-	mPath := filepath.Join(deploymentBasePath, m.ManifestDir)
-	log.Infof("Deploying Meshnet from: %s", mPath)
-	if err := execer.Exec("kubectl", "apply", "-k", mPath); err != nil {
-		return err
-	}
-	log.Infof("Meshnet Deployed")
-	return nil
+	d := &deploy.MeshnetSpec{}
+	return d.Deploy(ctx)
 }
 
 func (m *MeshnetSpec) Healthy(ctx context.Context) error {
-	log.Infof("Waiting on Meshnet to be Healthy")
-	w, err := m.kClient.AppsV1().DaemonSets("meshnet").Watch(ctx, metav1.ListOptions{
-		FieldSelector: fields.SelectorFromSet(fields.Set{metav1.ObjectNameField: "meshnet"}).String(),
-	})
-	if err != nil {
-		return err
-	}
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context canceled before healthy")
-		case e, ok := <-w.ResultChan():
-			if !ok {
-				return fmt.Errorf("watch channel closed before healthy")
-			}
-			d, ok := e.Object.(*appsv1.DaemonSet)
-			if !ok {
-				return fmt.Errorf("invalid object type: %T", d)
-			}
-			if d.Status.NumberReady == d.Status.DesiredNumberScheduled &&
-				d.Status.NumberUnavailable == 0 {
-				log.Infof("Meshnet Healthy")
-				return nil
-			}
-		}
-	}
+	h := &deploy.MeshnetSpec{}
+	return h.Healthy(ctx)
 }
