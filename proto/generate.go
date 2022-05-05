ec2e970a8ddc4cc0f1430fc4ed7a56d9300f9831
marcushines <80116818+marcushines@users.noreply.github.com>
2022-05-05 23:34:19 +0000

    create generate file for protos to keep them synced (#144)

---
+++ proto/generate.go
@@ -0,0 +1,4 @@
+package proto
+
+//go:generate protoc --go_out=./topo --go_opt=paths=source_relative ./topo.proto
+//go:generate protoc --go_out=./controller --go-grpc_out=./controller --go-grpc_opt=paths=source_relative --go_opt=paths=source_relative ./controller.proto
