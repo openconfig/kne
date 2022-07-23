6c8e93820759d256b6dea1adda49be7764fe0a24
Roman Dodin <dodin.roman@gmail.com>
2022-07-23 10:25:47 +0200

    added build step for kne cli and oci-push target

---
+++ .mk/ocipush.mk
@@ -0,0 +1,14 @@
+# build kne_cli and push it as an OCI artifact to ttl.sh
+# to obtain the pushed artifact use:
+# docker run --rm -v $(pwd):/workspace ghcr.io/deislabs/oras:v0.11.1 pull ttl.sh/<image-name>
+.PHONY: oci-push
+oci-push: build
+	@echo
+	
+	@echo "With the following pull command you get a kne binary at your working directory. To use this downloaded binary use - './kne <command>'\nMake sure to add ./ prefix in order to use the downloaded binary and not the globally installed kne"
+	
+	@echo 'If https proxy is configured in your environment, pass the proxies via --env HTTPS_PROXY="<proxy-address>" flag of the docker run command.'
+	
+# push to ttl.sh
+	@docker run --rm -v $$(pwd):/workspace ghcr.io/oras-project/oras:v0.12.0 push ttl.sh/kne-$(COMMIT):1d ./kne > /dev/null
+	@echo "download with: docker run --rm -v \$$(pwd):/workspace ghcr.io/oras-project/oras:v0.12.0 pull ttl.sh/kne-$(COMMIT):1d"
--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 MESHNET_DOCKER_IMAGE := hfam/meshnet
 GOPATH ?= ${HOME}/go
+KNE_CLI_BIN := kne
 
 COMMIT := $(shell git describe --dirty --always)
 TAG := $(shell git describe --tags --abbrev=0 || echo latest)
@@ -7,6 +8,7 @@ TAG := $(shell git describe --tags --abbrev=0 || echo latest)
 
 include .mk/kind.mk
 include .mk/lint.mk
+include .mk/ocipush.mk
 
 .PHONY: all
 all: docker
@@ -24,3 +26,8 @@ up: kind-start
 .PHONY: down
 ## Desroy test environment
 down: kind-stop
+
+.PHONY: build
+## Build kne
+build:
+	go build -o $(KNE_CLI_BIN) -ldflags="-s -w" kne_cli/main.go
