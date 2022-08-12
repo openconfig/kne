9204ba082f5cd22dd22863b84cd1ee506573dea6
alexmasi <alexmasi@google.com>
2022-08-12 12:56:39 -0700

    fix topo create

--- api/clientset/v1beta1/topology.go
+++ api/clientset/v1beta1/topology.go
@@ -129,6 +129,10 @@ func (t *topologyClient) Get(ctx context.Context, name string, opts metav1.GetOp
 }
 
 func (t *topologyClient) Create(ctx context.Context, topology *topologyv1.Topology, opts metav1.CreateOptions) (*topologyv1.Topology, error) {
+	topology.TypeMeta = metav1.TypeMeta{
+		Kind:       "Topology",
+		APIVersion: "networkop.co.uk/v1beta1",
+	}
 	obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(topology)
 	if err != nil {
 		return nil, fmt.Errorf("failed to convert topology to unstructured: %v", err)
