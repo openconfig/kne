420b39b9825d2df908878795e356a6f0050352c9
Oscar Frasier <ofrasier@arista.com>
2022-11-02 19:58:39 -0400

    Add vendor data, update arista operator to v2.0.1 (#253)
    - Add a vendor_data field to the node proto
        This allows the arista node to override feature toggles
        and block startup acknowledgement on certain internal processes.
    
    - Define an arista-specific config type
        This is in proto/ceos and define APIs for the above features
    
    - Upgrade to the v2.0.1 of the arista operator
        A new major version was needed for a breaking change related to
        testing. The existing uint32 fields did not serialize properly when
        using the dynamic fake, which was needed to test the CRD generation
        with vendor-specific data (and address a general coverage gap, too).
    
    - Switch to the arista operator dynamic client
        Previously, we were using the REST client.

--- proto/generate.go
+++ proto/generate.go
@@ -1,4 +1,5 @@
 package proto
 
 //go:generate protoc --go_out=./topo --go_opt=paths=source_relative ./topo.proto
+//go:generate protoc --go_out=./ceos --go_opt=paths=source_relative ./ceos.proto
 //go:generate protoc --go_out=./controller --go-grpc_out=./controller --go-grpc_opt=paths=source_relative --go_opt=paths=source_relative ./controller.proto
