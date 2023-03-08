0943f934028bf6c8ba3806411b0800a9d29d2467
Paul Borman <borman@google.com>
2023-03-08 17:17:04 -0600

    New packages exec and exec/fake.
    Exec provides is a wrapper around os/exec that is testable.
    It does not provide all of the functionality that os/exec provides
    but provides sufficient functionality for kne.

---
+++ exec/exec.go
@@ -0,0 +1,54 @@
+// Package exec wraps the os/exec package.  It facilitates testing by use of the sub-packge fake.
+//
+// Example
+//
+//	var buf bytes.Buffer
+//	c := Command("echo", "bob")
+//	c.SetStdout(&buf)
+//	err := c.Run()
+package exec
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+// A Cmd is an interface representing a command to run.
+type Cmd interface {
+	SetStdout(io.Writer) // Redirect standard output to the writer
+	SetStderr(io.Writer) // Redirect standard error to the writer
+	Run() error          // Operates the same as os/exec/Cmd.Run
+}
+
+// Command is a variable that normally points to NewCommand.
+// It is a variable so tests can redirect calls to a fake.
+var Command func(cmd string, args ...string) Cmd = NewCommand
+
+// NewCommand returns a Cmd that can run the supplied command.
+// NewCommand sets up stdout and stderr to go to os.Stdout and os.Stderr.
+// SetStdout and SetStderr are used to change where to send the output.
+//
+// Most programs should use exec.Command rather than exec.NewCommand.
+func NewCommand(cmd string, args ...string) Cmd {
+	c := exec.Command(cmd, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return command{cmd: c}
+}
+
+// A command wraps the standard os/exec Cmd type.  Since it only contains one
+// pointer we can pass it by value.
+type command struct {
+	cmd *exec.Cmd
+}
+
+func (c command) SetStdout(w io.Writer) { c.cmd.Stdout = w }
+func (c command) SetStderr(w io.Writer) { c.cmd.Stderr = w }
+func (c command) Run() error {
+	if err := c.cmd.Run(); err != nil {
+		return fmt.Errorf("%q failed: %v", c.cmd.String(), err)
+	}
+	return nil
+}
