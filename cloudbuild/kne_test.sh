e372ed524e0c0cc41e63d8026ca328108224b4a5
alexmasi <alexmasi@google.com>
2023-03-21 19:55:07 -0700

    remove interactive mode for cp

--- cloudbuild/kne_test.sh
+++ cloudbuild/kne_test.sh
@@ -86,7 +86,7 @@ kind delete cluster --name kne
 # Create a kubeadm single node cluster
 sudo kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock --pod-network-cidr 10.244.0.0/16
 mkdir -p "$HOME"/.kube
-sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo cp /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 kubectl apply -f "$HOME"/flannel/Documentation/kube-flannel.yml
 docker network create multinode
