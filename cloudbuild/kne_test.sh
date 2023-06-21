aa25e4f8369cebea16b0b5f4a1500fea3cce4e71
Alex Masi <a3masi@gmail.com>
2023-06-21 16:38:09 -0700

    Upgrade kind + k8 ver (#388)
    * Update k8s + kind + kind node ver
    
    * update go mod
    
    * fix vers
    
    * update presub
    
    * fix vendors presub

--- cloudbuild/kne_test.sh
+++ cloudbuild/kne_test.sh
@@ -51,7 +51,7 @@ cluster:
     name: kne
     recycle: True
     version: v0.17.0
-    image: 'kindest/node:v1.26.0'
+    image: 'kindest/node:v1.27.3'
     googleArtifactRegistries:
       - us-west1-docker.pkg.dev
     containerImages:
