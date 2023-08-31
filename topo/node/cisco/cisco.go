79c90c2e1b6d3788cfaed5877fadc84f1076d56d
Neha Manjunath <nehamanjunath@google.com>
2023-08-31 21:29:09 +0000

    Resolve comment

--- topo/node/cisco/cisco.go
+++ topo/node/cisco/cisco.go
@@ -423,7 +423,7 @@ func defaults(pb *tpb.Node) (*tpb.Node, error) {
 		if pb.Config.Image == "" {
 			pb.Config.Image = "xrd:latest"
 		}
-		if pb.HostConstraints == nil {
+		if len(pb.HostConstraints) == 0 {
 			pb.HostConstraints = append(pb.HostConstraints,
 				&tpb.HostConstraint{Constraint: &tpb.HostConstraint_KernelConstraint{
 					KernelConstraint: &tpb.KernelParam{Name: "fs.inotify.max_user_instances",
@@ -438,7 +438,7 @@ func defaults(pb *tpb.Node) (*tpb.Node, error) {
 		if pb.Config.Image == "" {
 			pb.Config.Image = "8000e:latest"
 		}
-		if pb.HostConstraints == nil {
+		if len(pb.HostConstraints) == 0 {
 			pb.HostConstraints = append(pb.HostConstraints,
 				&tpb.HostConstraint{Constraint: &tpb.HostConstraint_KernelConstraint{
 					KernelConstraint: &tpb.KernelParam{Name: "kernel.pid_max",
