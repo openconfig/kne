3877a90cf322561591906d5a6396ee202ef496e1
Ed Beaty <ebeaty@cisco.com>
2025-04-24 07:34:14 -0700

    .

--- topo/node/cisco/cisco_test.go
+++ topo/node/cisco/cisco_test.go
@@ -1088,6 +1088,10 @@ func TestPushCfg(t *testing.T) {
 			}
 			defer fp.Close()
 			err = n.ConfigPush(context.Background(), fp)
+
+			if tt.wantErr && err != "foo" {
+				t.Fatal("Expecting error foo, got error %v \n", err)
+			}
 			if tt.wantErr && err == nil {
 				t.Fatal("Expecting an error, but no error is raised \n")
 			}
