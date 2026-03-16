dda7424a7e016fee79e75532e42325b7c15b6a9b
Pranay Maddula <pranaysameer@google.com>
2026-03-16 12:04:01 +0000

    Add support for IN_CLUSTER_PROXY node type - Update proto/topo.proto with `IN_CLUSTER_PROXY` in Vendor and `Node.Type` enums. - Create `topo/node/inclusterproxy` package implementing `Node` interface. - Set default image to `nicolaka/netshoot:latest` and enforce at least 1 `Service`. - Enforce link constraints to allow exactly one interface (`eth1`). - Enforce that `proxy-pool-for` label is present in static configuration. - Add static validation assuring `eth1` is connected directly to the node given in `proxy-pool-for`. - Add automatic `socat` command generation using `peer-ip` and `target-port` labels:     - Supports IPv4 address allocation arithmetic using `/31` masks.     - Adds IPv6 support using `/127` arithmetic and handles `TCP6-LISTEN` argument switches. - Update `topo/topo.go` to register the new node type. - Update `topo/topo.go` to register the new node type. - Add unit tests in `topo/node/inclusterproxy/inclusterproxy_test.go`.

---
+++ topo/node/inclusterproxy/inclusterproxy_test.go
@@ -0,0 +1,280 @@
+package inclusterproxy
+
+import (
+	"testing"
+
+	"github.com/openconfig/gnmi/errdiff"
+	topopb "github.com/openconfig/kne/proto/topo"
+	"github.com/openconfig/kne/topo/node"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		desc    string
+		ni      *node.Impl
+		wantPB  *topopb.Node
+		wantErr string
+	}{{
+		desc:    "nil node impl",
+		wantErr: "nodeImpl cannot be nil",
+	}, {
+		desc:    "nil pb",
+		wantErr: "nodeImpl.Proto cannot be nil",
+		ni:      &node.Impl{},
+	}, {
+		desc: "missing proxy-pool-for label",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+			},
+		},
+		wantErr: "label 'proxy-pool-for' is required",
+	}, {
+		desc: "missing interfaces",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "exactly one interface is required",
+	}, {
+		desc: "too many interfaces",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {},
+					"eth2": {},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "exactly one interface is required",
+	}, {
+		desc: "wrong interface name",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth2": {},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "interface must be 'eth1'",
+	}, {
+		desc: "mismatched peer name",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "wrong_dut"},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "eth1 must be connected to dut1, found wrong_dut",
+	}, {
+		desc: "multiple services not allowed",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{
+					1: {Inside: 1},
+					2: {Inside: 2},
+				},
+			},
+		},
+		wantErr: "exactly one service must be configured",
+	}, {
+		desc: "valid pb manual command",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{1790: {Inside: 1790}},
+				Config: &topopb.Config{
+					Command: []string{"socat"},
+				},
+			},
+		},
+		wantPB: &topopb.Node{
+			Name: "test_node",
+			Labels: map[string]string{
+				"proxy-pool-for": "dut1",
+			},
+			Config: &topopb.Config{
+				Image:   "nicolaka/netshoot:latest",
+				Command: []string{"socat"},
+			},
+			Interfaces: map[string]*topopb.Interface{
+				"eth1": {PeerName: "dut1"},
+			},
+			Services: map[uint32]*topopb.Service{1790: {Inside: 1790}},
+		},
+	}, {
+		desc: "valid pb with automatic generation ipv4",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+					"peer-ip":        "192.168.100.1/31",
+					"target-port":    "179",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{
+					1790: {Inside: 1790},
+				},
+			},
+		},
+		wantPB: &topopb.Node{
+			Name: "test_node",
+			Labels: map[string]string{
+				"proxy-pool-for": "dut1",
+				"peer-ip":        "192.168.100.1/31",
+				"target-port":    "179",
+			},
+			Config: &topopb.Config{
+				Image:   "nicolaka/netshoot:latest",
+				Command: []string{"/bin/sh", "-c"},
+				Args:    []string{"ip addr add 192.168.100.0/31 dev eth1 && ip link set eth1 up && sleep 2 && /usr/bin/socat -d -d TCP-LISTEN:1790,reuseaddr,fork TCP:192.168.100.1:179"},
+			},
+			Interfaces: map[string]*topopb.Interface{
+				"eth1": {PeerName: "dut1"},
+			},
+			Services: map[uint32]*topopb.Service{
+				1790: {Inside: 1790},
+			},
+		},
+	}, {
+		desc: "valid pb with automatic generation ipv6",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+					"peer-ip":        "2001:db8::1/127",
+					"target-port":    "179",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{
+					1790: {Inside: 1790},
+				},
+			},
+		},
+		wantPB: &topopb.Node{
+			Name: "test_node",
+			Labels: map[string]string{
+				"proxy-pool-for": "dut1",
+				"peer-ip":        "2001:db8::1/127",
+				"target-port":    "179",
+			},
+			Config: &topopb.Config{
+				Image:   "nicolaka/netshoot:latest",
+				Command: []string{"/bin/sh", "-c"},
+				Args:    []string{"ip addr add 2001:db8::0/127 dev eth1 && ip link set eth1 up && sleep 2 && /usr/bin/socat -d -d TCP6-LISTEN:1790,reuseaddr,fork TCP6:2001:db8::1:179"},
+			},
+			Interfaces: map[string]*topopb.Interface{
+				"eth1": {PeerName: "dut1"},
+			},
+			Services: map[uint32]*topopb.Service{
+				1790: {Inside: 1790},
+			},
+		},
+	}, {
+		desc: "invalid peer-ip format",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+					"peer-ip":        "invalid-cidr",
+					"target-port":    "179",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "invalid 'peer-ip' label",
+	}, {
+		desc: "invalid peer-ip mask v4",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+					"peer-ip":        "192.168.100.1/24",
+					"target-port":    "179",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "only /31 mask is supported for IPv4",
+	}, {
+		desc: "invalid peer-ip mask v6",
+		ni: &node.Impl{
+			Proto: &topopb.Node{
+				Name: "test_node",
+				Labels: map[string]string{
+					"proxy-pool-for": "dut1",
+					"peer-ip":        "2001:db8::1/64",
+					"target-port":    "179",
+				},
+				Interfaces: map[string]*topopb.Interface{
+					"eth1": {PeerName: "dut1"},
+				},
+				Services: map[uint32]*topopb.Service{1: {Inside: 1}},
+			},
+		},
+		wantErr: "only /127 mask is supported for IPv6",
+	}}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			impl, err := New(tt.ni)
+			if s := errdiff.Substring(err, tt.wantErr); s != "" {
+				t.Fatalf("unexpected error: got: %v, want: %s", err, s)
+			}
+			if tt.wantErr != "" {
+				return
+			}
+			if !proto.Equal(impl.GetProto(), tt.wantPB) {
+				t.Fatalf("New() failed: got\n%v\nwant\n%v", impl.GetProto(), tt.wantPB)
+			}
+		})
+	}
+}
